ef7f7c02e84588424ee3db6ff4fa557df89d3014
SriharshaMandru <mandrusriharsha@gmail.com>
2024-06-21 13:37:24 +0530

    Testimonial slider files added

---
+++ testimonial.js
@@ -0,0 +1,26 @@
+const testimonials = [{
+    name:"Eva sawyer",
+    job:"CEO, Fashworks",
+    image: "",
+    testimonial:
+    "Neque voluptas ac tincidus vitae semper quis lectus nulla at voluptas diam ut venenatis telus in metus vulptate eu scelerisque felis imperdiet proin "
+},
+{
+    name:"Katey Topaz",
+    job:"Developer, TechCrew",
+    image: "",
+    testimonial:"Neque voluptas ac tincidus vitae semper quis lectus nulla at voluptas diam ut venenatis telus in metus vulptate eu scelerisque felis imperdiet proin "
+},
+{
+    name:"Nicola Blakely",
+    job:"CEO, Zeal Wheels",
+    image: "",
+    testimonial:"Neque voluptas ac tincidus vitae semper quis lectus nulla at voluptas diam ut venenatis telus in metus vulptate eu scelerisque felis imperdiet proin "
+}
+];
+let i = 0;
+let j = testimonials.length;
+
+let testimonialContainer = document.getElementById("testimonial-container");
+let nextBtn = document.getElementById("next");
+let prevBtn = document.getElementById("prev");
